5f3e31d42a757cb9e26cdb2f08a0dda9d22b8839
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-27 16:25:12 +0100

    [UPD] Set countdown e display-none

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@ const countdown = document.getElementById('countdown')
 const instructions = document.getElementById('instructions')
 const numbersList = document.getElementById('numbers-list')
 
+//Start Game: countdown 
+
+//Definizione della variabile i per mostrare secondi
+let i = 0;
+countdown.innerText = i; 
+
+const interval = setInterval(() => {
+    i++;
+    countdown.innerText = i;
+}, 1000);
+
+setTimeout(() => {
+    clearInterval(interval)
+    countdown.classList.add('d-none')
+    instructions.classList.add('d-none')
+}, 4000);
