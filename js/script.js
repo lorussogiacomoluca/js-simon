d04f082b09ade660c7b5fbac8fa16814a552eba7
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-27 16:35:32 +0100

    [UPD] Random num generation

--- js/script.js
+++ js/script.js
@@ -3,6 +3,26 @@ const countdown = document.getElementById('countdown')
 const instructions = document.getElementById('instructions')
 const numbersList = document.getElementById('numbers-list')
 
+
+
+//Funzione per generare 5 numeri casuali
+function getRandInt(max, times){
+    //Array per salvare i numeri casuali
+    const randoms = []
+
+    //Ciclo FOR per salvare i numeri generati
+    for(let i=0; i<times; i++){
+        randoms.push(Math.floor(Math.random()*max)) 
+    }
+
+    //Return ARRAY
+    return randoms;
+}
+
+//Generazione numeri casuali
+const gameRandoms = getRandInt(100,5)
+
+
 //Start Game: countdown 
 
 //Definizione della variabile i per mostrare secondi
