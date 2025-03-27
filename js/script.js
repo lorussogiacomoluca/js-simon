183df6223ddb34b23d41a580c56c26f6429df332
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-03-27 16:14:56 +0100

    [UPD] Variables - First Part

---
+++ js/script.js
@@ -0,0 +1,5 @@
+//Definizione variabili - PART 1
+const countdown = document.getElementById('countdown')
+const instructions = document.getElementById('instructions')
+const numbersList = document.getElementById('numbers-list')
+
